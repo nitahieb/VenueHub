f8b4e6ffd35851d511254e60608abab6056a0b0f
Nitahi-silverend <Nitahi-silverend@outlook.com>
2025-09-19 15:38:26 -0700

    Deleted venues.ts

--- src/data/venues.ts
+++
@@ -1,3 +0,0 @@
-// This file is now deprecated - all venue data comes from the database
-// Import from src/utils/venues.ts instead
-export { getAllVenues, getFeaturedVenues, searchVenues } from '../utils/venues';
