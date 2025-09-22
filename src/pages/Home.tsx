36741527891bfd0e325a57f9bc348c2807cbaccb
Nitahi-silverend <Nitahi-silverend@outlook.com>
2025-09-21 21:37:21 -0700

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,7 +61,7 @@ const Home: React.FC = () => {
                 to="/venues"
                 className="inline-flex items-center bg-gradient-to-r from-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-800 text-white px-8 py-3 rounded-xl font-semibold shadow-lg hover:shadow-xl transition-all duration-300"
               >
-                <Search className="h-5 w-5" />
+                <Search className="ml-2 h-5 w-5" />
                 <span>Browse All Venues</span>
               </Link>
             </div>
